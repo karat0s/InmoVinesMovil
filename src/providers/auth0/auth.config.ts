9b6b2a1d28ade37534cae4e9424b6110cf2e42bb
Daniel Arroyo <daniarromay@gmail.com>
2018-06-05 16:12:27 +0200

    qwe

--- src/providers/auth0/auth.config.ts
+++ src/providers/auth0/auth.config.ts
@@ -4,6 +4,5 @@ export const AUTH_CONFIG = {
   // Needed for Auth0Cordova (capitalization: Id):
   clientId: 'k91VENmQE5BlNnazCEYgFJj5aD7Dz2IT',
   domain: 'karatos.eu.auth0.com',
-  callbackURL: location.href,
   packageIdentifier: 'com.inmovinesmovil' // config.xml widget ID, e.g., com.auth0.ionic
 };
